2bd82a36eaa0b9b8f981b0090a65d78e1c0aee89
nuhakhangr8 <nuhakhangr8@gmail.com>
2023-05-18 03:26:20 +0530

    Add middlewares

--- d8-web-scrapping/index.js
+++ d8-web-scrapping/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT  = 1337;
 
+//middlewares
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}))
+
 app.listen(PORT,()=>{
     console.log("server is running")
 })
