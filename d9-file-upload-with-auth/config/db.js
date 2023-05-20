ca2ad75fb7df092ac8791184efc9ef10afbba2f0
nuhakhangr8 <nuhakhangr8@gmail.com>
2023-05-21 04:22:02 +0530

    Database setup

---
+++ d9-file-upload-with-auth/config/db.js
@@ -0,0 +1,16 @@
+const {Sequelize} = require("sequelize");
+
+const createDB = new Sequelize ("test-db", "user","password" ,{
+    dialect:sqlite,
+    host:"./config/db.sqlite"
+})
+
+const connectToDB = createDB.sync().then((res)=>{
+    console.log("connected to DB")
+})
+.catch((e)=>{
+    console.log("db connection failed",e)});
+
+module.exports = {createDB, connectToDB};
+
+    
