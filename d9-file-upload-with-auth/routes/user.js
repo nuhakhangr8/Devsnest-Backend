7d78814089751f801a469c528dbf9f2442461a55
nuhakhangr8 <nuhakhangr8@gmail.com>
2023-05-24 01:51:32 +0530

    create sign up functionality

--- d9-file-upload-with-auth/routes/user.js
+++ d9-file-upload-with-auth/routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const validators{
     validateName,
@@ -11,8 +13,37 @@ router.post("/signup",async(req,res)=>{
     try{
         const { name, email, password, isSeller} = req.body;
         
-        const existingUser = await 
+        const existingUser = await User.findOne({ where: email})
+        if(existingUser){
+            return res.status(403).json({
+                err:"User exists",
+            })
+        }
 
+        if (!validateName(name)){
+            return res.status(400).json({err:"name validate fails"})
+        }
+        if (!validateEmail(email)){
+            return res.status(400).json({err:"email validate fails"})
+        }
+        if (!validatePassword(password)){
+            return res.status(400).json({err:"password validate fails"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password);
+
+        const user = {
+            name,
+            email,
+            isSeller,
+            password:hashedPassword,
+        }
+
+        const createdUser = await User.create(user);
+
+        return res.status(201).json({
+            message:`welcome ${createdUser.name}`,
+        })
     } catch(e){
         res.status(500).send(e);
     }
