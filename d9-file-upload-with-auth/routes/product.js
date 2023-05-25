9ad313f647a7fd713e30f0dcdd2f2faff44f2661
nuhakhangr8 <nuhakhangr8@gmail.com>
2023-05-26 02:24:54 +0530

    refactor: changed file path to middlewares

--- d9-file-upload-with-auth/routes/product.js
+++ d9-file-upload-with-auth/routes/product.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {isAuthenticated,isSeller} = require("../utils/auth");
+const {isAuthenticated,isSeller} = require("../middlewares/auth");
 
 router.post("/create",isAuthenticated,isSeller,async(req,res)=>{
-    
+
 } );
 
 module.exports = router;
